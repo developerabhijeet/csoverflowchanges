a3049a13064d029d180e1eba3d73da163d955dde
developerabhijeet <abtiwari@bestpeers.com>
2021-04-04 16:59:18 +0530

    unable to fetch error.

--- client/src/redux/actions/users/userActions.jsx
+++ client/src/redux/actions/users/userActions.jsx
@@ -1,5 +1,5 @@
 //causing change of state in application
-import { USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from './actionTypes';
+import { USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS,USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from './actionTypes';
 import axios from 'axios';
 const signupuserAction = (name, email, password, bio, jobtitle, tech) => {
   return async dispatch => {
@@ -37,4 +37,41 @@ const signupuserAction = (name, email, password, bio, jobtitle, tech) => {
     }
   };
 };
-export { signupuserAction };
+
+//Login Action 
+
+const loginUserAction = (email,password)=>{
+  return async dispatch =>{
+    try{
+      dispatch({
+        type: USER_LOGIN_REQUEST,
+      });
+
+      const config={
+        headers: {
+          "Content-Type": "application/JSON"
+        }
+      }
+    const {data} = await axios.post('http://localhost:4000/app/login',
+    {email,password},
+     config
+     );
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+    console.log(data);
+    //saving user to localStorage
+    localStorage.setItem('userAuthData', JSON.stringify(data));
+   
+    }catch(error){
+      console.log(error)
+      dispatch({
+        type: USER_LOGIN_FAIL,
+        payload: error.response && error.response.data.message,
+      });
+    }
+  };
+};
+
+export { signupuserAction, loginUserAction };
